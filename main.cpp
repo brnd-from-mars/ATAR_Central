b49f19284a8fa3248916eb28649bf53c27246316
Brendan Berg <brendan_berg@aol.com>
2019-02-12 11:18:37 +0100

    Add AVR config to Cmake file
    The Cmake file was extended with the paths to all AVR related programs
    and config options to select necessary board options.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
+#include <util/delay.h>
+#include <avr/io.h>
 
 
 int main ()
 {
-    std::cout << "Hello, World!" << std::endl;
+    DDRB |= (1 << PB7);
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true)
+    {
+        PORTB |= 1 << PB7;
+        _delay_ms(1000);
+        PORTB &= ~(1 << PB7);
+        _delay_ms(500);
+    }
+#pragma clang diagnostic pop
+
     return 0;
 }
