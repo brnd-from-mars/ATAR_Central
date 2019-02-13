a40694358c91a4ac9eb9cd846dd71e8a4fff3c54
Brendan Berg <brendan_berg@aol.com>
2019-02-13 09:23:04 +0100

    Add AVR_Lib as submodule
    The AVR_Lib repository is used for all AVR functions to make the
    implementation versatile for all used microcontrollers on the robot.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include <util/delay.h>
 #include <avr/io.h>
 
+#include "mcu/io/DigitalOutputPin.hpp"
+
 
 int main ()
 {
     DDRB |= (1 << PB7);
+    DigitalOutputPin dop(&PORTB, PB7);
+
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true)
     {
-        PORTB |= 1 << PB7;
-        _delay_ms(1000);
-        PORTB &= ~(1 << PB7);
-        _delay_ms(500);
+        dop.Enable();
+        _delay_ms(50);
+        dop.Disable();
+        _delay_ms(100);
     }
 #pragma clang diagnostic pop
 
+
+#pragma ide diagnostic push
+#pragma ide diagnostic ignored "OCDFAInspection"
     return 0;
+#pragma ide diagnostic pop
+
 }
