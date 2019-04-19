7720d26c27d59b9bcead89a7bf7df83a93c07a89
Brendan Berg <brendan_berg@aol.com>
2019-04-20 00:57:08 +0200

    Add timer component to AVR_Lib
    Main functionality of the AVR timers were abstracted into the timer
    component in the AVR_Lib. Just like in the USART component, config
    structures were created. The Timer class can control 8bit and 16bit
    timers. A PWM class was inherited. Timer interrupt handlers might be
    added in the future.
    
    A AnalogOutputPin class was created by using the newly implemented PWM
    abstraction.

--- main.cpp
+++ main.cpp
@@ -4,37 +4,37 @@
 #include "mcu/io/DigitalOutputPin.hpp"
 #include "mcu/io/AnalogInputPin.hpp"
 #include "mcu/io/AnalogInputController.hpp"
+#include "mcu/io/AnalogOutputPin.hpp"
 #include "mcu/usart/UARTConnection.hpp"
+
+#include "mcu/timer/Timer.hpp"
+#include "mcu/timer/TimerFastPWM.hpp"
 
 
 int main ()
 {
-    AnalogInputController aic;
-    AnalogInputPin aip1(7);
-
-    DigitalOutputPin dop(&PORTB, PB7);
-
     sei();
 
-    UARTConnection uartUSB(0, 115200);
+    TimerFastPWM timer0 (0, 256);
+    TimerFastPWM timer1 (1, 256);
+    TimerFastPWM timer3 (3, 1);
 
-    aic.EnableAnalogController();
+    AnalogOutputPin aop0(&PORTB, PB7, &timer0, 0);
+    AnalogOutputPin aop1(&PORTB, PB6, &timer1, 1);
+    AnalogOutputPin aop2(&PORTE, PE5, &timer3, 2);
 
-    int i = 1000;
+    timer0.Enable();
+    timer1.Enable();
+    timer3.Enable();
 
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true)
     {
-        uartUSB.WriteByte(aip1.GetValue());
-        _delay_ms(1);
-        --i;
-        if (i == 0)
-        {
-            dop.Flip();
-            i = 1000;
-        }
+        aop0.SetValue(0x9f);
+        aop1.SetValue(0x5f);
+        aop2.SetValue(0x8f);
     }
 #pragma clang diagnostic pop
 
