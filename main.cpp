741252e938734f35c112d368b54ff09ecc7a552e
Brendan Berg <brendan_berg@aol.com>
2019-04-22 23:56:25 +0200

    Improve UART interrupt communication
    The UART interrupt master connection class was improved with a enhanced
    transmission protocol.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 #include <util/delay.h>
 
 #include "mcu/io/AnalogInputController.hpp"
+#include "mcu/io/AnalogInputPin.hpp"
 
 #include "mcu/usart/UARTConnection.hpp"
 #include "mcu/usart/UARTInterruptMasterConnection.hpp"
 
 #include "mcu/timer/TimerFastPWM.hpp"
-
-#include "mcu/interrupt/ExternalInterruptHandler.hpp"
 
 #include "periph/Motor.hpp"
 #include "periph/LightEmittingDiode.hpp"
@@ -16,68 +15,72 @@
 
 int main ()
 {
-    UARTConnection uartUSB(0, 9600);
-    UARTConnection uartGamma(2, 38400);
-    UARTInterruptMasterConnection uartAlpha(1, 38400, &PORTH, PH5);
-    UARTInterruptMasterConnection uartBeta(3, 38400, &PORTH, PH6);
+    UARTConnection uartUSB(0x00, 38400);
+//    UARTConnection uartGamma(0x02, 38400);
+//    UARTConnection uartAlpha(0x01, 38400);
+    UARTInterruptMasterConnection uartAlpha(0x01, 38400, &PORTH, PH5);
+//    UARTInterruptMasterConnection uartBeta(0x03, 38400, &PORTH, PH6);
 
     SingleColorLED ledHeartbeat;
     ledHeartbeat.RegisterPin(&PORTB, PB7);
 
-    SingleColorLED ledGroundIllumination;
-    ledGroundIllumination.RegisterPin(&PORTH, PH4);
+//    // TODO: outsource
+//    SingleColorLED ledGroundIllumination;
+//    ledGroundIllumination.RegisterPin(&PORTH, PH4);
 
-    AnalogInputController analogInputController;
+//    AnalogInputController analogInputController;
+//
+//    // TODO: outsource
+//    AnalogInputPin groundSensorLeft(0x07);
+//    AnalogInputPin groundSensorRight(0x06);
+//    analogInputController.RegisterAnalogPin(&groundSensorLeft);
+//    analogInputController.RegisterAnalogPin(&groundSensorRight);
+//
+//    // TODO: outsource
+//    AnalogInputPin batteryLevelSensor(0x05);
+//    analogInputController.RegisterAnalogPin(&batteryLevelSensor);
+//
+//    analogInputController.Enable();
 
-    AnalogInputPin groundSensorLeft(7);
-    AnalogInputPin groundSensorRight(6);
-    analogInputController.RegisterAnalogPin(&groundSensorLeft);
-    analogInputController.RegisterAnalogPin(&groundSensorRight);
-
-    AnalogInputPin batteryLevelSensor(5);
-    analogInputController.RegisterAnalogPin(&batteryLevelSensor);
-
-    analogInputController.Enable();
-
-    TimerFastPWM timer0(0, 1);
-    TimerFastPWM timer3(3, 1);
-    TimerFastPWM timer4(4, 1);
-
-    Motor motorRF(&PORTL, PL0, &PORTL, PL2, &PINL, PL4, &PINL, PL6,
-                  &PORTE, PE3, &timer3, 0);
-
-    Motor motorRB(&PORTB, PB2, &PORTB, PB0, &PING, PG2, &PING, PG0,
-                  &PORTH, PH3, &timer4, 0);
-
-    Motor motorLB(&PORTA, PA1, &PORTA, PA3, &PINC, PC2, &PINC, PC0,
-                  &PORTE, PE5, &timer3, 2);
-
-    Motor motorLF(&PORTA, PA7, &PORTA, PA5, &PINC, PC4, &PINC, PC6,
-                  &PORTG, PG5, &timer0, 1);
-
-    timer0.Enable();
-    timer3.Enable();
-    timer4.Enable();
+//    TimerFastPWM timer0(0x00, 0x0001);
+//    TimerFastPWM timer3(0x03, 0x0001);
+//    TimerFastPWM timer4(0x04, 0x0001);
+//
+//    Motor motorRF(&PORTL, PL0, &PORTL, PL2, &PINL, PL4, &PINL, PL6,
+//                  &PORTE, PE3, &timer3, 0x00);
+//
+//    Motor motorRB(&PORTB, PB2, &PORTB, PB0, &PING, PG2, &PING, PG0,
+//                  &PORTH, PH3, &timer4, 0x00);
+//
+//    Motor motorLB(&PORTA, PA1, &PORTA, PA3, &PINC, PC2, &PINC, PC0,
+//                  &PORTE, PE5, &timer3, 0x02);
+//
+//    Motor motorLF(&PORTA, PA7, &PORTA, PA5, &PINC, PC4, &PINC, PC6,
+//                  &PORTG, PG5, &timer0, 0x01);
+//
+//    timer0.Enable();
+//    timer3.Enable();
+//    timer4.Enable();
 
     sei();
+
+    _delay_ms(2000);
 
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true)
     {
-        uartAlpha.Update();
-        uartBeta.Update();
+        uartAlpha.RequestData();
+        while (!uartAlpha.ResponseReceived())
+        {
+            uartAlpha.Update();
+        }
+        uartUSB.WriteString(uartAlpha.GetMessage(),
+            uartAlpha.GetMessageLength());
 
-        motorRF.Update();
-        motorRB.Update();
-        motorLB.Update();
-        motorLF.Update();
-
-        _delay_ms(1000);
-        ledHeartbeat.Enable();
-        _delay_ms(1000);
-        ledHeartbeat.Disable();
+        uartAlpha.WriteByte('1');
+        _delay_ms(100);
     }
 #pragma clang diagnostic pop
 
